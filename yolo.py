7cec26481789dc805e8662541d2cf101cde97c3e
jaygamer888 <jaygamer888@gmail.com>
2014-10-02 16:52:28 -0400

    jaymazing

---
+++ yolo.py
@@ -0,0 +1,21 @@
+'''
+moveForward(100);
+turnRight(90);
+moveForward(100);
+turnRight(180);
+'''
+
+from turtle import Turtle
+
+jay = Turtle()
+jay.pensize(7)
+jay.pencolor('gold')
+jay.forward(100)
+jay.right(90)
+jay.forward(100)
+jay.right(50)
+jay.left(20)
+jay.left(67)
+jay.right(00)
+jay.forward(50)
+jay.left(25)
