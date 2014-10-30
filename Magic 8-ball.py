7367c138ecf5fcb58b444817c2c09e687508bb13
jaygamer888 <jaygamer888@gmail.com>
2014-10-30 16:55:10 -0400

    made magic  8-ball

---
+++ Magic 8-ball.py
@@ -0,0 +1,28 @@
+
+import random
+
+print('Welcome to Magic 8-Ball')
+
+while True:
+
+    print('enter your Question')
+    question = input('> ')
+
+
+    print("you're question is...",question)
+    answers = [
+        'YES.',
+        'NO.',
+        'could happen.',
+        'possibly.',
+        'lets find out.',
+        'I will be waiting.',
+        'Best not to know.',
+        'oh,heck no!.',
+        'you bet.',
+        'shut up',
+        'back off.',
+    ]
+
+    answer = random.choice(answers)
+    print(answer)
